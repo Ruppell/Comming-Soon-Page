6f61312d4633ca185d50e80229cd8b69dbae1e38
Vernon Grant <vernon@ruppell.io>
2019-04-30 22:37:16 +0800

    Added Pair Pair Full Branding.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
     // Set some date in the future. In this case, it's always Jan 1
     // new Date(year, monthIndex [, day [, hours [, minutes [, seconds [, milliseconds]]]]])
-    var futureDate  = new Date(currentDate.getFullYear(), 5, 11);
+    var futureDate  = new Date(currentDate.getFullYear(), 5, 1);
 
     // Calculate the difference in seconds between the future and current date
     var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
